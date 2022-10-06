86a577502c5ccc471a47a4fd6b9ec6e6f2fb487c
MaximGromak <maximgromak@gmail.com>
2022-10-06 20:56:14 +1000

    init commit

---
+++ src/test/java/com/example/skillbox_sprboot_project/SkillboxSprbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skillbox_sprboot_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillboxSprbootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
